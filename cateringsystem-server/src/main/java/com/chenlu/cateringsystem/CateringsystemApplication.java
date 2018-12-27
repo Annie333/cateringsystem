a9a4634531e7dc3a1ed67f2f158091120b1258b2
Annie333 <39488270@qq.com>
2018-12-27 13:16:34 +0800

    Initial commit

---
+++ cateringsystem-server/src/main/java/com/chenlu/cateringsystem/CateringsystemApplication.java
@@ -0,0 +1,14 @@
+package com.chenlu.cateringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringsystemApplication.class, args);
+	}
+
+}
+
---
+++ cateringsystem-server/src/test/java/com/chenlu/cateringsystem/CateringsystemApplicationTests.java
@@ -0,0 +1,17 @@
+package com.chenlu.cateringsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CateringsystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
